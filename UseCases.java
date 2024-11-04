09af9d3545d0588a6d0c1762d7b50b0e83cdeaf4
tinngo56 <tinngo56@iastate.edu>
2024-11-04 16:54:37 -0600

    checkpoint

--- UseCases.java
+++ UseCases.java
@@ -1,18 +1,28 @@
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class UseCases {
     public static void main(String[] args) {
-        /*Room room = new Room(1, 2, "Deluxe", 150.0, "Available");
-        Booking booking = new Booking(1, "2023-10-01", "2023-10-05", 600.0, "Paid", room);
-        Hotel hotel = new Hotel(1, "Grand Hotel", "New York", 100, 5000.0, 20000.0, 4.5);
-
-        System.out.println("Booking ID: " + booking.getId());
-        System.out.println("Hotel Name: " + hotel.getName());
-        System.out.println("Room Type: " + booking.getRoom().getRoomType());*/
+        /*
+         * Room room = new Room(1, 2, "Deluxe", 150.0, "Available");
+         * Booking booking = new Booking(1, "2023-10-01", "2023-10-05", 600.0, "Paid",
+         * room);
+         * Hotel hotel = new Hotel(1, "Grand Hotel", "New York", 100, 5000.0, 20000.0,
+         * 4.5);
+         * 
+         * System.out.println("Booking ID: " + booking.getId());
+         * System.out.println("Hotel Name: " + hotel.getName());
+         * System.out.println("Room Type: " + booking.getRoom().getRoomType());
+         */
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,7 +30,7 @@ public class UseCases {
                 5000.0, 20000.0, 4.5, 5);
         ArrayList<Room> rooms = GenerateRooms(hotel);
 
-        while(true) {
+        while (true) {
             System.out.println("\nWelcome! Choose an actor: \n");
             PrintActors();
 
@@ -46,6 +56,7 @@ public class UseCases {
 
     /**
      * Generates some pre-defined rooms
+     * 
      * @param hotel The hotel in which the rooms belong to
      * @return List of rooms
      */
@@ -69,9 +80,10 @@ public class UseCases {
 
     /**
      * Use cases for customer
+     * 
      * @param scanner main loop scanner
-     * @param hotel hotel for the use case
-     * @param rooms list of rooms for the user case
+     * @param hotel   hotel for the use case
+     * @param rooms   list of rooms for the user case
      */
     public static void CustomerScenario(Scanner scanner, Hotel hotel, ArrayList<Room> rooms) {
         Customer customer = new Customer(1, "Bob", "bob@gmail.com", "Silver",
@@ -82,9 +94,9 @@ public class UseCases {
         int action = scanner.nextInt();
 
         // BOOK HOTEL ROOM
-        if(action == 1) {
+        if (action == 1) {
             System.out.println("\n\n----- BOOK A HOTEL ROOM -----\n");
-            if(hotel.getNumAvailableRooms() <= 0) {
+            if (hotel.getNumAvailableRooms() <= 0) {
                 System.out.println("Hotel is sold out!");
                 return;
             }
@@ -96,13 +108,13 @@ public class UseCases {
             System.out.print("What room type? (Standard, Deluxe or Suite):  ");
             String roomType = scanner.next();
 
-            for(Room room : rooms) {
+            for (Room room : rooms) {
                 // if room is found matching request
-                if(room.getRoomType().equals(roomType) && room.getBedCount() == beds) {
+                if (room.getRoomType().equals(roomType) && room.getBedCount() == beds) {
                     System.out.print("There is a room for $" + room.getPricePerNight() + " per night. " +
                             "Purchase? (y/n) ");
                     char answer = scanner.next().charAt(0);
-                    if(answer == 'y') {
+                    if (answer == 'y') {
                         System.out.print("How many nights? ");
                         int nights = scanner.nextInt();
                         LocalDate checkoutDate = LocalDate.now().plusDays(nights);
@@ -114,14 +126,13 @@ public class UseCases {
                     }
                 }
             }
-            if(!didBook) {
+            if (!didBook) {
                 System.out.println("Could not find a room. Try again with different requirements.");
             }
 
             // CHECK OUT OF HOTEL ROOM
         } else {
             System.out.println("\n\n----- CHECK OUT OF A HOTEL ROOM -----\n");
-
 
         }
     }
