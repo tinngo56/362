5a19a782daaeaeb5700353835229fc0155fbc2d6
tinngo56 <tinngo56@iastate.edu>
2024-11-06 22:50:37 -0600

    remove lock

--- Models/Mappable.java
+++ Models/Mappable.java
@@ -8,12 +8,13 @@ import java.util.Map;
 public abstract class Mappable<T> {
     /**
      * Convert the object to a Map representation using reflection
+     * 
      * @return Map containing the object's properties
      */
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
         Field[] fields = this.getClass().getDeclaredFields();
-        
+
         try {
             for (Field field : fields) {
                 field.setAccessible(true);
@@ -25,12 +26,13 @@ public abstract class Mappable<T> {
         } catch (IllegalAccessException e) {
             throw new RuntimeException("Error converting object to map", e);
         }
-        
+
         return map;
     }
 
     /**
      * Convert a Map back to an object of type T using reflection
+     * 
      * @param map The map containing the object's properties
      * @return An instance of type T
      */
@@ -39,27 +41,27 @@ public abstract class Mappable<T> {
         try {
             // Get the actual class of T
             Class<T> clazz = (Class<T>) this.getClass();
-            
+
             // Create a new instance using the default constructor
             Constructor<T> constructor = clazz.getDeclaredConstructor();
             constructor.setAccessible(true);
             T instance = constructor.newInstance();
-            
+
             // Get all declared fields
             Field[] fields = clazz.getDeclaredFields();
-            
+
             // Set each field from the map
             for (Field field : fields) {
                 field.setAccessible(true);
                 Object value = map.get(field.getName());
-                
+
                 if (value != null) {
                     // Handle primitive type conversions
                     Class<?> fieldType = field.getType();
-                    
+
                     if (value instanceof Number) {
                         Number numValue = (Number) value;
-                        
+
                         if (fieldType == Integer.class || fieldType == int.class) {
                             field.set(instance, numValue.intValue());
                         } else if (fieldType == Long.class || fieldType == long.class) {
@@ -82,7 +84,7 @@ public abstract class Mappable<T> {
                     }
                 }
             }
-            
+
             return instance;
         } catch (Exception e) {
             throw new RuntimeException("Error converting map to object", e);
